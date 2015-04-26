c3bc026e1bb6ccbbbd63261492eeda935aa496a8
@mafiswana <sinndlovu@clientele.co.za>
2015-04-26 20:22:13 +0200

    itext

--- iTextSharpPDF/Form1.Designer.cs
+++ iTextSharpPDF/Form1.Designer.cs
@@ -28,19 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonTwainBib_Click = new System.Windows.Forms.Button();
             this.btnExport_Click = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // buttonTwainBib_Click
-            // 
-            this.buttonTwainBib_Click.Location = new System.Drawing.Point(12, 21);
-            this.buttonTwainBib_Click.Name = "buttonTwainBib_Click";
-            this.buttonTwainBib_Click.Size = new System.Drawing.Size(123, 102);
-            this.buttonTwainBib_Click.TabIndex = 0;
-            this.buttonTwainBib_Click.Text = "TwainBib";
-            this.buttonTwainBib_Click.UseVisualStyleBackColor = true;
-            this.buttonTwainBib_Click.Click += new System.EventHandler(this.buttonTwainBib_Click_Click);
             // 
             // btnExport_Click
             // 
@@ -58,7 +47,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(321, 144);
             this.Controls.Add(this.btnExport_Click);
-            this.Controls.Add(this.buttonTwainBib_Click);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -67,7 +55,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button buttonTwainBib_Click;
         private System.Windows.Forms.Button btnExport_Click;
     }
 }
